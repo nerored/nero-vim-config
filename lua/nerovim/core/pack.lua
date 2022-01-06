118134ca046f9389256e24d4223bc8aa6ddcf75b
nerored <nero_stellar@icloud.com>
2022-01-06 23:01:24 +0800

    remove print

--- lua/nerovim/core/pack.lua
+++ lua/nerovim/core/pack.lua
@@ -10,14 +10,8 @@ local packer_url = 'https://github.com/wbthomason/packer.nvim'
 function packer:init()
 	local install_path = os.getenv("DAT_PATH") .. "site/pack/packer/start/packer.nvim"
 	if vim.fn.isdirectory(install_path) == 0 then
-		local output = vim.api.nvim_exec(
-			string.format('!git clone --depth 1 %s %s',packer_url,install_path),
-		true)
-
-		if string.len(output) > 0 then
-			print("err:" .. output)
-			print("please restart and try again")
-		end
+		vim.api.nvim_command(
+			string.format('!git clone --depth 1 %s %s',packer_url,install_path))
 	end
 	return self
 end
